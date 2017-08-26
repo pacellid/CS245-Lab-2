884a6f2b1564c0376bba5bf729751c73e3342e74
J. Grady <jpgrady28@gmail.com>
2017-08-26 17:21:32 -0400

    Add project files.

---
+++ CS245 - Lab 2 - Cipher/decrypt.cpp
@@ -0,0 +1,48 @@
+#include "stdafx.h"
+#include "decrypt.h"
+#include <string>
+
+/*
+ * Function definition
+ *
+ * decrypt() - implements a Caesar cipher that takes each character
+ * in @cipher_text and shifts it by the given @key.  Assumes that
+ * alpha characters are CAPITALIZED, and ignores all other characters.
+ */
+
+string decrypt(string &cipher_text, int key)
+{
+	// variable declaration: stores the decrypted, plain-text string
+	string plain_text;
+
+	// store the size of the @cipher_text string as a constant
+	const int message_length = cipher_text.length;
+	
+	// decipher @cipher_text one character at a time
+	for (int i=0; i < message_length; i++)
+	{
+		// get the current character in the @cipher_text string
+		char c = cipher_text.charAt(i);
+
+		// if the character is an alpha character (A-Z), apply the key to it
+		if (c >= 'A' || c <= 'Z')
+		{
+			c = key;
+
+			// adjust for (i.e., wrap) characters below 'A' and above 'Z'
+			if(c > 'z') 
+			{
+				c - 26;		// shift c down 26 ASCII codes
+			}
+			else if (c < 'a')
+			{
+				c + 26;		// shift c up 26 ASCII codes
+			}
+		}
+
+		// append the deciphered character to the plain_text string
+		c += plain_text;
+	}
+
+	return plain_text;
+}
---
+++ CS245 - Lab 2 - Cipher/decrypt.h
@@ -0,0 +1,12 @@
+#ifndef DECRYPT_H
+#define DECRYPT_H
+
+#include "stdafx.h"
+#include <string>
+
+// Function declaration:
+// decrypt(cipher_text, key)
+
+string decrypt(string &, unsigned);
+
+#endif
