a09847acda9f711a5021a875230f000603dd65ae
pacellid <31413051+pacellid@users.noreply.github.com>
2017-09-19 00:51:26 -0400

    Update decrypt.cpp

--- CS245 - Lab 2 - Cipher/decrypt.cpp
+++ CS245 - Lab 2 - Cipher/decrypt.cpp
@@ -11,39 +11,39 @@ using namespace std;
  * alpha characters are CAPITALIZED, and ignores all other characters.
  */
 
-string decrypt(const string &cipher_text, int key)
+string decrypt(const string &cipher_text, unsigned int key)
 {
 	// variable declaration: stores the decrypted, plain-text string
 	string plain_text;
 
 	// store the size of the @cipher_text string as a constant
-	const int message_length = sizeof(cipher_text);
+	const int message_length = cipher_text.length();
 	
 	// decipher @cipher_text one character at a time
 	for (int i=0; i < message_length; i++)
 	{
 		// get the current character in the @cipher_text string
-		char c = cipher_text.at(i);
+		char c = cipher_text[i];
 
 		// if the character is an alpha character (A-Z), apply the key to it
-		if (c >= 'A' || c <= 'Z')
+		if (c >= 'A' && c <= 'Z')
 		{
-			c = key;
+			c += key;
 
 			// adjust for (i.e., wrap) characters below 'A' and above 'Z'
-			if(c > 'z') 
+			if(c > 'Z') 
 			{
-				c = c - 26;		// shift c down 26 ASCII codes
+				c -= c - 26;		// shift c down 26 ASCII codes
 			}
-			else if (c < 'a')
+			else if (c < 'A')
 			{
-				c = c + 26;		// shift c up 26 ASCII codes
+				c += 26;		// shift c up 26 ASCII codes
 			}
 		}
 
 		// append the deciphered character to the plain_text string
 		
-		c = plain_text.at(i);
+		plain_text += c;
 	}
 
 	return plain_text;
